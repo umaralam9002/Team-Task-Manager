9ba9db23d6101cb7a0f6a3eb5bc3be817bd6c4cd
umaralam <umaralam9002@gmail.com>
2025-12-07 23:06:52 +0500

    Manage Task Working (on Tabs)

--- src/pages/Admin/ManageTasks.jsx
+++ src/pages/Admin/ManageTasks.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosinstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { LuFileSpreadsheet } from 'react-icons/lu';
+import TaskStatusTabs from '../../components/TaskStatusTabs';
+import TaskCard from '../../components/Cards/TaskCard';
 
 const ManageTasks = () => {
     const [allTasks, setAllTasks] = useState([]);
@@ -69,8 +71,45 @@ const ManageTasks = () => {
               </button>
             </div>
 
+            {tabs?.[0]?.count > 0 && (
+              <div className="flex items-center gap-3">
+                <TaskStatusTabs
+                  tabs={tabs}
+                  activeTab={filterStatus}
+                  setActiveTab={setFilterStatus}
+                />
+
+                <button className="hidden lg:flex download-btn" onClick={handleDownloadReport}>
+                  <LuFileSpreadsheet className="text-lg" />
+                  Download Report
+                </button>
+              </div>
+            )}
             
           </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+              {allTasks?.map((item, index) => (
+                <TaskCard
+                  key={item._id}
+                  title={item.title}
+                  description={item.description}
+                  priority={item.priority}
+                  status={item.status}
+                  progress={item.progress}
+                  createdAt={item.createdAt}
+                  dueDate={item.dueDate}
+                  assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+                  attachmentCount={item.attachments?.length || 0}
+                  completedTodoCount={item.completedTodoCount || 0}
+                  todoChecklist={item.todoChecklist || []}
+                  onClick={() => {
+                    handleClick(item);
+                  }}
+                />
+              ))}
+           </div>
+
         </div>
       </DashboardLayout>
   )
